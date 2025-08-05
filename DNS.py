71b45f298cdce527597a402ccfb42af303575d1d
Carlos_souza <royal_green@localhost.localdomain>
2025-08-05 08:53:56 -0300

    busca por wordlist

--- DNS.py
+++ DNS.py
@@ -1,15 +1,16 @@
 import socket
 import sys
 
-if len(sys.argv) == 2:
+if len(sys.argv) == 3:
     dominio = sys.argv[1]
-    brute = ["ns1", "ns2", "ns3", "ns4", "www", "ftp", "mail", "intranet"]
-
-    for nome in brute:
+    wordlist = sys.argv[2]
+    with open(wordlist, 'r') as arquivo:
+        bruteforce = arquivo.read().splitlines()
+    for nome in bruteforce:
         DNS = nome + "." + dominio
         try:
             print(DNS + ': ' + socket.gethostbyname(DNS))
         except socket.gaierror:
             pass
 else:
-    print('Modo de uso: python DNS.py <dominio>')
+    print('Modo de uso: python DNS.py <wordlist> <wordlist>')
