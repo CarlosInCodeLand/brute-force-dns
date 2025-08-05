475c54ebb7d4444ec99c855250d8b60f3e5042a8
Carlos_souza <royal_green@localhost.localdomain>
2025-08-05 09:22:25 -0300

    color

--- DNS.py
+++ DNS.py
@@ -1,16 +1,20 @@
 import socket
 import sys
+from colorama import init, Fore, Back, Style
+
+init(autoreset=True)
 
 if len(sys.argv) == 3:
     dominio = sys.argv[1]
     wordlist = sys.argv[2]
     with open(wordlist, 'r') as arquivo:
         bruteforce = arquivo.read().splitlines()
+        print(Fore.BLUE +"Iniciando varredura . . .")
     for nome in bruteforce:
         DNS = nome + "." + dominio
         try:
-            print(DNS + ': ' + socket.gethostbyname(DNS))
+            print(f'{Fore.LIGHTBLUE_EX + "Dominio Encontrado >>>> "}{Fore.GREEN +DNS + ": " + Fore.LIGHTGREEN_EX + Style.BRIGHT + socket.gethostbyname(DNS)}')
         except socket.gaierror:
             pass
 else:
-    print('Modo de uso: python DNS.py <wordlist> <wordlist>')
+    print(Fore.RED + 'Modo de uso: python DNS.py <wordlist> <wordlist>')
