ed81750c54aeffa83ac1b8164899b0415545bc4d
Carlos_souza <royal_green@localhost.localdomain>
2025-08-05 08:29:35 -0300

    sys.agrv

--- DNS.py
+++ DNS.py
@@ -1,11 +1,15 @@
 import socket
+import sys
 
-dominio = input("alvo: ")
-brute = ["ns1", "ns2", "ns3", "ns4", "www", "ftp", "mail", "intranet"]
+if len(sys.argv) == 2:
+    dominio = sys.argv[1]
+    brute = ["ns1", "ns2", "ns3", "ns4", "www", "ftp", "mail", "intranet"]
 
-for nome in brute:
-    DNS = nome + "." + dominio
-    try:
-        print(DNS + ': ' + socket.gethostbyname(DNS))
-    except socket.gaierror:
-        pass
+    for nome in brute:
+        DNS = nome + "." + dominio
+        try:
+            print(DNS + ': ' + socket.gethostbyname(DNS))
+        except socket.gaierror:
+            pass
+else:
+    print('Modo de uso: python DNS.py <dominio>')
