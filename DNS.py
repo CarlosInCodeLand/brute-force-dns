ec1d81be9f0e0b8e2dc5d64422d3d6e1ccfa75db
Carlos_souza <royal_green@localhost.localdomain>
2025-08-06 08:33:10 -0300

    function

--- DNS.py
+++ DNS.py
@@ -1,18 +1,19 @@
 import socket
 import sys
-from colorama import init, Fore, Back, Style
+from colorama import Fore, Style
 
-if len(sys.argv) == 3:
-    dominio = sys.argv[1]
-    wordlist = sys.argv[2]
-    with open(wordlist, 'r') as arquivo:
-        bruteforce = arquivo.read().splitlines()
-        print(Fore.BLUE +"Iniciando varredura . . .")
-    for nome in bruteforce:
-        DNS = nome + "." + dominio
-        try:
-            print(f'{Fore.LIGHTBLUE_EX + "Dominio Encontrado >>>> "}{Fore.GREEN +DNS + ": " + Fore.LIGHTGREEN_EX + Style.BRIGHT + socket.gethostbyname(DNS)}')
-        except socket.gaierror:
-            pass
-else:
-    print(Fore.RED + 'Modo de uso: python DNS.py <wordlist> <wordlist>')
+def brute_force():
+    if len(sys.argv) == 3:
+        dominio = sys.argv[1]
+        wordlist = sys.argv[2]
+        with open(wordlist, 'r') as arquivo:
+            bruteforce = arquivo.read().splitlines()
+            print(Fore.BLUE +"Iniciando varredura . . .")
+        for nome in bruteforce:
+            DNS = nome + "." + dominio
+            try:
+                print(f'{Fore.LIGHTBLUE_EX + "Dominio Encontrado >>>> "}{Fore.GREEN +DNS + ": " + Fore.LIGHTGREEN_EX + Style.BRIGHT + socket.gethostbyname(DNS)}')
+            except socket.gaierror:
+                pass
+    else:
+        print(Fore.RED + 'Modo de uso: python DNS.py <dominio> <wordlist>')
